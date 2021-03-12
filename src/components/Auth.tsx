00ca202e7c5614bce6e2621fcd3ca65c04e504c3
hiro_from_22 <ore097miki@gmail.com>
2021-03-13 01:23:20 +0900

    メールとパスワードでログインする機能を作りました

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -41,6 +41,25 @@ const useStyles = makeStyles((theme) => ({
 
 const Auth: React.FC = () => {
   const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const signInEmail = async () => {
+    await auth.signInWithEmailAndPassword(email, password);
+  };
+
+  const signUpEmail = async () => {
+    await auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  const signInGoogle = async () => {
+    // auth.signInWithPopup(provider)とだけ書くと Promiseでラップされたオブジェクトがreturnされる
+    // Promiseオブジェクトはthenを続けて書かないと、中身のデータが見ることはできない
+    // async awaitを使うことでPromiseを取り払うことができる
+    // awaitの影響により、auth.signInWithPopup(provider)が実行されるまでは次の処理が実行されない
+    await auth.signInWithPopup(provider).catch((err) => alert(err.message));
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -50,7 +69,7 @@ const Auth: React.FC = () => {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          {isLogin ? "Login" : "Register"}
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
@@ -63,6 +82,10 @@ const Auth: React.FC = () => {
             name="email"
             autoComplete="email"
             autoFocus
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEmail(e.target.value);
+            }}
           />
           <TextField
             variant="outlined"
@@ -74,13 +97,52 @@ const Auth: React.FC = () => {
             type="password"
             id="password"
             autoComplete="current-password"
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setPassword(e.target.value);
+            }}
           />
           <Button
-            type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={
+              isLogin
+                ? async () => {
+                    try {
+                      await signInEmail();
+                    } catch (err) {
+                      alert(err.message);
+                    }
+                  }
+                : async () => {
+                    try {
+                      await signUpEmail();
+                    } catch (err) {
+                      alert(err.message);
+                    }
+                  }
+            }
+          >
+            {isLogin ? "Login" : "Register"}
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <span className={styles.login_reset}>Forgot password?</span>
+            </Grid>
+            <Grid item xs>
+              <span onClick={() => setIsLogin(!isLogin)}>
+                {isLogin ? "Create new account?" : "Back to login"}
+              </span>
+            </Grid>
+          </Grid>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={signInGoogle}
           >
             Sign In
           </Button>
