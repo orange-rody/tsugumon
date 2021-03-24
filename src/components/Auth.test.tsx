8ddbe07fd8c2d26ca5954d60835521636cfd4b78
hiro_from_22 <ore097miki@gmail.com>
2021-03-25 01:07:00 +0900

    authentication Authコンポーネントのテストコードを書きました
    レンダリングテストとinputフォームの入力が適正に行われているかのテストを行いました

---
+++ src/components/Auth.test.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Auth from "./Auth";
+
+// itの後に毎回行う処理はafterEachで設定することができる
+// cleanupを実行するとrender()でレンダリングされた内容をアンマウントすることができる
+afterEach(() => cleanup());
+
+describe("Rendering", () => {
+  it("Should render the elements correctly", () => {
+    render(<Auth />);
+    expect(screen.getByTestId("header")).toBeTruthy();
+    expect(screen.getByTestId("avatar")).toBeTruthy();
+    expect(screen.getByTestId("main")).toBeTruthy();
+    expect(screen.getByTestId("email")).toBeTruthy();
+    expect(screen.getByTestId("password")).toBeTruthy();
+    expect(screen.getByTestId("toggle-button")).toBeTruthy();
+    expect(screen.getAllByRole("button")[0]).toBeTruthy();
+    expect(screen.getByTestId("login")).toBeTruthy();
+    expect(screen.getByTestId("guide-for-sign-up")).toBeTruthy();
+    expect(screen.getByTestId("guide-for-password")).toBeTruthy();
+    expect(screen.getByTestId("test-user-button")).toBeTruthy();
+    expect(screen.getByTestId("footer")).toBeTruthy();
+  });
+});
+
+describe("Input form onChange Event", () => {
+  it("Should update input value correctly", () => {
+    render(<Auth />);
+    const inputEmail = screen.getAllByRole("textbox")[0];
+    userEvent.type(inputEmail, "test");
+    expect(inputEmail.value).toBe("test");
+    const inputPassword = screen.getByLabelText("パスワード");
+    userEvent.type(inputPassword, "test");
+    expect(inputPassword.value).toBe("test");
+    // userEvent.type(inputPassword,"")
+  });
+});
