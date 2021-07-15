127f4a7209d8e5389b1fd349a162d8f507c9ea9e
hiro_from_22 <ore097miki@gmail.com>
2021-07-16 02:14:59 +0900

    tab-bar:Material-UIを使用してタブバーの叩き台を作りました。
    tabをクリックすると、画面が切り替わるのを手動で確認済みです。
    デザインは未調整なので、今後CSSで設計していきたいと思います。

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,47 +1,119 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import { makeStyles, Theme } from "@material-ui/core/styles";
 import { auth } from "../firebase";
 import CreatePost from "./CreatePost/CreatePost";
 import Wrapper from "./Parts/Wrapper";
 import PaperContainer from "./Parts/PaperContainer";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import {
   HomeRounded,
   SearchRounded,
   AddBoxRounded,
   NotificationsRounded,
-  PersonRounded,
+  Person,
 } from "@material-ui/icons";
-import TabBar from "./Parts/TabBar";
+
+interface TabPanelProps {
+  children?: JSX.Element;
+  index: string;
+  value: string;
+}
+
+const TabPanel = (props: TabPanelProps) => {
+  const { children, index, value, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabPanel-${index}`}
+      aria-labelledby={index}
+      {...other}
+    >
+      {value === index && children}
+    </div>
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 const MainView = () => {
-  const [element, setElement] = useState("Home");
-  const signOut = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    auth.signOut();
+  const [value, setValue] = useState("home");
+  const classes = useStyles();
+  const handleChange = (e: React.ChangeEvent<{}>, newValue: string) => {
+    setValue(newValue);
   };
-  const Element = () => {
-    switch (element) {
-      case "Home":
-        return (
-          <TabBar title="ホーム">
-            <HomeRounded />
-          </TabBar>
-        );
-      case "CreatePost":
-        return <CreatePost />;
-      default:
-        return (
-          <>
-            <p>Home</p>
-          </>
-        );
-    }
-  };
+
   return (
-    <Wrapper>
-      <PaperContainer>
-        <Element />
-      </PaperContainer>
-    </Wrapper>
+    <div className={classes.root}>
+      <Wrapper>
+        <PaperContainer>
+          <AppBar position="fixed" color="default">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              aria-label="tabs"
+            >
+              <Tab
+                label="ホーム"
+                icon={<HomeRounded />}
+                value="home"
+                aria-controls="tabPanel-home"
+              />
+              <Tab
+                label="検索"
+                icon={<SearchRounded />}
+                value="search"
+                aria-controls="tabPanel-search"
+              />
+              <Tab
+                label="追加"
+                icon={<AddBoxRounded />}
+                value="add"
+                aria-controls="tabPanel-add"
+              />
+              <Tab
+                label="通知"
+                icon={<NotificationsRounded />}
+                value="notification"
+                aria-controls="tabPanel-notification"
+              />
+              <Tab
+                label="プロフィール"
+                icon={<Person />}
+                value="profile"
+                aria-controls="profile-notification"
+              />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index="home">
+            <p>Home</p>
+          </TabPanel>
+          <TabPanel value={value} index="search">
+            <p>Search</p>
+          </TabPanel>
+          <TabPanel value={value} index="add">
+            <CreatePost />
+          </TabPanel>
+          <TabPanel value={value} index="notification">
+            <p>Notification</p>
+          </TabPanel>
+          <TabPanel value={value} index="profile">
+            <p>Profile</p>
+          </TabPanel>
+        </PaperContainer>
+      </Wrapper>
+    </div>
   );
 };
 
--- src/components/Parts/TabBar.tsx
+++ src/components/Parts/TabBar.tsx
@@ -1,70 +1,81 @@
 import React from "react";
-import styled from "styled-components";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import {
+  HomeRounded,
+  SearchRounded,
+  AddBoxRounded,
+  NotificationsRounded,
+  PersonRounded,
+} from "@material-ui/icons";
 
-const TabWrapper = styled.div`
-  position: absolute;
-  botom: 0;
-  width: 100%;
-  height: 49px;
-  border-top: hsl(0, 0, 80%);
-`;
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
-const Label = styled.label`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  flex-flow: row;
-  cursor: pointer;
-`;
+export default function TabsButton() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-const Tab = styled.span`
-  display: block;
-  width: 20%;
-  height: 100%;
-`;
+  
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
 
-const Icon = styled.p`
-  color: hsl(0, 0, 20%);
-  transition: all 100ms;
-`;
-
-const IconTitle = styled.p`
-  width: 100%;
-  height: 20px;
-  font-size: 12px;
-  color: hsl(0, 0, 20%);
-  transition: all 100ms;
-`;
-
-const Input = styled.input`
-  visibility: hidden;
-  &:checked + ${Icon} {
-    color: #4fc0ad;
-  }
-  + ${IconTitle} {
-    color: #4fc0ad;
-  }
-`;
-
-type Props = {
-  children: JSX.Element;
-  title: string;
-};
-
-const TabBar = (props: Props) => {
-  // const [active, setActive] = useState("home");
   return (
-    <TabWrapper>
-      <Label>
-        <Input type="radio">
-          <Tab>
-            <Icon>{props.children}</Icon>
-            <IconTitle>{props.title}</IconTitle>
-          </Tab>
-        </Input>
-      </Label>
-    </TabWrapper>
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="on"
+          indicatorColor="primary"
+          textColor="primary"
+          aria-label="scrollable force tabs example"
+        >
+          {/* NOTE >> Tabコンポーネントではchilderenがサポートされていない。
+                      そのため、タブに表示する内容はlabelとiconで指定することとなる */}
+          {/* NOTE >> Tabコンポーネント上で関数tabPropsを展開することで、必要なpropsを
+                      Tabコンポーネントに渡している。*/}
+          <Tab
+            id="home"
+            data-testid="home"
+            label="Home"
+            icon={<HomeRounded />}
+          />
+          <Tab
+            id="search"
+            data-testid="search"
+            label="Search"
+            icon={<SearchRounded />}
+          />
+          <Tab
+            id="post"
+            data-testid="post"
+            label="Post"
+            icon={<AddBoxRounded />}
+          />
+          <Tab
+            id="notification"
+            data-testid="notification"
+            label="Notification"
+            icon={<NotificationsRounded />}
+          />
+          <Tab
+            id="profile"
+            data-testid="profile"
+            label="Profile"
+            icon={<PersonRounded />}
+          />
+        </Tabs>
+      </AppBar>
+    </div>
   );
-};
-
-export default TabBar;
+}
