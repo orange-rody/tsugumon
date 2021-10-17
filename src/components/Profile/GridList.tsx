efe1fdcffc0c48490afab36ae12624ea2187e64a
hiro_from_22 <ore097miki@gmail.com>
2021-10-17 14:46:06 +0900

    profile:読み込むボタンでページネーション機能を実現しました.

--- src/components/Profile/GridList.tsx
+++ src/components/Profile/GridList.tsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../features/userSlice";
 import { db } from "../../firebase";
-import InfiniteScroll from "react-infinite-scroller";
 import styled from "styled-components";
 import mediaQuery from "styled-media-query";
+import DefaultButton from "../Parts/DefaultButton";
 
 const mediaMobile = mediaQuery.lessThan("medium");
 
@@ -25,7 +25,8 @@ const PhotoList = styled.ul`
   justify-content: space-between;
   margin: 0;
   padding: 0;
-  margin-bottom: 60px;
+  padding-bottom: 60px;
+  overflow: scroll;
   list-style: none;
 `;
 
@@ -65,7 +66,7 @@ const Grid: React.FC = () => {
   const currentTime: number = new Date().getTime();
   const [posts, setPosts] = useState<Post[]>([]);
   const [oldestPostId, setOldestPostId] = useState("");
-  const [hasMore, setHasMore] = useState(false);
+  const [loading, setLoading] = useState(false);
   const unsubscribes = useRef<Unsubscribe[]>([]);
 
   // NOTE >> コレクション「posts」の中でtimestampの値が最小のドキュメント(最も古い投稿)のidを取得する。
@@ -82,55 +83,33 @@ const Grid: React.FC = () => {
       });
   }
 
-  // NOTE >> 過去の投稿（5件）を読み込む関数を定義する。
-  const postLoader = async(time: number) => {
-    await getOldestPostId();
-    db.collection("posts")
-      .where("uid", "==", uid)
-      .orderBy("timestamp", "desc")
-      .startAfter(time)
-      .limit(15)
-      .onSnapshot((snapshot) => {
-        handleSnapshot(snapshot);
-        console.log("postLoaderが実行されました");
-      });
-  };
-
   const unsubscribe = () => {
     db.collection("posts").where("uid", "==", uid).orderBy("timestamp", "desc");
   };
 
-  // NOTE >> 初回の読み込みを行う関数を定義する。
-  const initialLoad = () => {
-    // NOTE >> 変数「unsubscribe」に関数unsubscribeを追加し、実行する
-    // QUESTION >> onSnapshot()は一度実行したら、バックグラウンドでずっと監視と
-    //             更新処理を続ける事になるのだろうか？だとしたら、
-    //             previousPostLoaderが起動するたび、onSnapshot()の
-    //             バックグラウンド処理が増え続けていくことになるのであって、
-    //             計算の負荷が急増してしまう。
-    //             だから、previousPostLoader()をunsubscribesの配列に格納し、
-    //             forEach()を使って、onSnapshot()の解除をループしているのではないか？
-    postLoader(currentTime);
-    unsubscribes.current.push(unsubscribe);
-  };
-
-  // NOTE >> スクロール時に投稿（15件）を追加で読み込む関数を定義する。
-  const additionalLoad = () => {
-    if (posts.length > 0) {
-      const lastPostedTime = posts[posts.length - 1].timestamp;
-      postLoader(lastPostedTime);
+  // NOTE >> 過去の投稿（15件）を読み込む関数を定義する。
+  async function postLoader(time: number) {
+    try {
+      db.collection("posts")
+        .where("uid", "==", uid)
+        .orderBy("timestamp", "desc")
+        .startAfter(time)
+        .limit(15)
+        .onSnapshot((snapshot) => {
+          handleSnapshot(snapshot);
+          console.log("postLoaderが実行されました");
+        });
+      // NOTE >> 変数「unsubscribe」に関数unsubscribeを追加し、実行する
+      // QUESTION >> onSnapshot()は一度実行したら、バックグラウンドでずっと監視と
+      //             更新処理を続ける事になる。postLoaderが起動するたび、
+      //             onSnapshot()のバックグラウンド処理が増え計算の負荷が増加して
+      //             しまうため、forEach()を使って、unsubscribesに格納したonSnapshot()を
+      //             解除するループを回している。
       unsubscribes.current.push(unsubscribe);
+    } catch (e) {
+      console.log(e);
     }
-    console.log("additionalLoadが実行されました。");
-  };
-
-  // NOTE >> onSnapshot()の監視・更新を解除するための関数を定義する。
-  const clear = () => {
-    unsubscribes.current.forEach((unsubscribe) => {
-      unsubscribe();
-      console.log("clearが実行されました。");
-    });
-  };
+  }
 
   // NOTE >> added,removed,modifiedのそれぞれの変更種類に応じて、
   //         処理を切り分けるようにする。
@@ -138,7 +117,6 @@ const Grid: React.FC = () => {
     let added: Post[] = [];
     let removed: Post[] = [];
     let modified: Post[] = [];
-    console.log("handleSnapshot was Called.");
     snapshot.docChanges().forEach((change: any) => {
       const post = {
         // NOTE >> ドキュメントのIDはdoc.idで取得し、フィールドの値は
@@ -148,19 +126,15 @@ const Grid: React.FC = () => {
       } as Post;
       console.log(post);
       if (change.type === "added") {
-        // NOTE >> onSnapshot()で「added」されるドキュメントは最新のものから
-        //         並ぶ必要があるため、push()を使用する。
         added.push(post);
       } else if (change.type === "removed") {
         removed.push(post);
-        console.log(removed);
       } else if (change.type === "modified") {
         modified.push(post);
-        console.log(modified);
       }
     });
     if (added.length > 0) {
-      setPosts([...added, ...posts]);
+      setPosts([...posts, ...added]);
     }
     if (removed.length > 0) {
       return;
@@ -172,17 +146,44 @@ const Grid: React.FC = () => {
             (find) => find.id === before.id
           );
           if (after) {
-            console.log(after);
             return after;
           } else {
-            console.log(before);
             return before;
           }
         });
       });
-      console.log(posts);
     }
   }
+
+  // NOTE >> 初回の読み込みを行う関数を定義する。
+  function initialLoad() {
+    setLoading(true);
+    getOldestPostId();
+    postLoader(currentTime).then(() => setLoading(false));
+    unsubscribes.current.push(unsubscribe);
+  }
+
+  // NOTE >> 追加の読み込みを行う関数を定義する。
+  function nextLoad() {
+    setLoading(true);
+    if (posts.length > 0) {
+      if (posts.find((find) => find.id === oldestPostId)) {
+        setLoading(false);
+        console.log("You are up to date!");
+      } else {
+        const lastPostedTime = posts[posts.length - 1].timestamp;
+        postLoader(lastPostedTime).then(() => setLoading(false));
+      }
+    }
+  }
+
+  // NOTE >> onSnapshot()の監視・更新を解除するための関数を定義する。
+  const clear = () => {
+    unsubscribes.current.forEach((unsubscribe) => {
+      unsubscribe();
+      console.log("clearが実行されました。");
+    });
+  };
 
   // async function checkHasMore() {
   //   await initialLoad();
@@ -205,11 +206,12 @@ const Grid: React.FC = () => {
   //         //         idを持つpostが含まれていたら、false(追加読み込みを中止)を、含まれて
   //         //         いなかったらtrue(追加読み込みを許可)を返す。
   //         setHasMore(
-  //           !Boolean(
-  //             posts.find((post: Post) => post.id === querySnapshot.docs[0].id)
-  //           )
+  //           posts.find((post: Post) => post.id === querySnapshot.docs[0].id) ===
+  //             undefined
   //         );
-  //         console.log(`setHasMoreが実行されました。setHasMoreの結果は${hasMore}でした。`);
+  //         console.log(
+  //           `setHasMoreが実行されました。setHasMoreの結果は${hasMore}でした。`
+  //         );
   //       } else {
   //         return;
   //       }
@@ -218,17 +220,13 @@ const Grid: React.FC = () => {
 
   // NOTE >> コンポーネントのライフサイクルに応じた処理をuseEffect()で指定する。
   useEffect(() => {
-    checkHasMore();
+    initialLoad();
     console.log(posts);
     // NOTE >> Unmountの際に、onSnapshot()の監視・更新処理を解除するようにする。
     return () => {
       clear();
     };
   }, []);
-
-  // useEffect(() => {
-  //   checkHasMore();
-  // }, []);
 
   const lastLine = posts.length % 3;
   function justifyLastLine() {
@@ -250,18 +248,27 @@ const Grid: React.FC = () => {
   return (
     <>
       <Main>
-        <InfiniteScroll hasMore={hasMore} loadMore={additionalLoad}>
-          <PhotoList>
-            {posts.map((post, index) => {
-              return (
-                <PhotoItem style={{ backgroundColor: "gray" }} key={index}>
-                  <PhotoImage src={post.imageUrl} />
-                </PhotoItem>
-              );
-            })}
-            {justifyLastLine()}
-          </PhotoList>
-        </InfiniteScroll>
+        <PhotoList>
+          {posts.map((post, index) => {
+            return (
+              <PhotoItem style={{ backgroundColor: "gray" }} key={index}>
+                <PhotoImage src={post.imageUrl} />
+              </PhotoItem>
+            );
+          })}
+          {justifyLastLine()}
+          <div>
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              <DefaultButton
+                child="読み込む"
+                onClick={nextLoad}
+                dataTestId="buttonForLoading"
+              />
+            )}
+          </div>
+        </PhotoList>
       </Main>
     </>
   );
